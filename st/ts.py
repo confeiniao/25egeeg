e0e91420a11c2951bb2a5359ed9d1f1d34befa54
serverless-100009112398 <coding_support@tencent.com>
2024-07-13 00:06:42 +0800

    更新文件 ts.py

--- st/ts.py
+++ st/ts.py
@@ -1,9 +1,8 @@
-import time
+# coding=utf-8
 import requests
-import socket
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from tqdm import tqdm
 import dns.resolver
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import tqdm
 
 lines_to_keep = []
 
@@ -17,7 +16,7 @@ def fetch_url(url):
             response = requests.get(url)
             response.raise_for_status()
             return response.text
-        except requests.exceptions.RequestException:
+        except requests.exceptions.RequestException as e:
             retries += 1
             if retries == max_retries - 2:
                 url = url.replace('mirror.ghproxy', 'gh-proxy')
@@ -36,34 +35,20 @@ def process_filter(url):
                 lines_to_keep.append(line)
 
 def check_dns_resolution(domain):
-    max_retries = 3
-    retries = 0
-    while retries < max_retries:
-        try:
-            resolver = dns.resolver.Resolver()
-            result = resolver.resolve(domain, 'A')
-            ips = [r.address for r in result]
-            return ips[0] if ips else None
-        except dns.resolver.NoAnswer:
-            retries += 1
-            if retries < max_retries:
-                time.sleep(5)  # 每次重试前等待 5 秒
-            else:
-                return None
-        except dns.resolver.NXDOMAIN:
-            return None
-        except dns.resolver.Timeout:
-            retries += 1
-            if retries < max_retries:
-                time.sleep(5)  # 每次重试前等待 5 秒
-            else:
-                return None
-        except dns.resolver.NoNameservers:
-            retries += 1
-            if retries < max_retries:
-                time.sleep(5)  # 每次重试前等待 5 秒
-            else:
-                return None
+    try:
+        resolver = dns.resolver.Resolver()
+        answers = resolver.query(domain)
+        ips = [answer.address for answer in answers]
+        return ips
+    except dns.resolver.NoAnswer:
+        return None
+    except dns.resolver.NXDOMAIN:
+        return None
+    except dns.resolver.NoNameservers:
+        return None
+    except Exception as e:
+        print(f"Error resolving {domain}: {str(e)}")
+        return None
 
 def filter_domains(lines):
     lines.sort(key=len)
@@ -74,38 +59,45 @@ def filter_domains(lines):
     unique_lines = sorted(unique_lines)
     return unique_lines
 
-if __name__ == '__main__':
-    urls = [
-        'https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt',
-        'https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts'
-    ]
+def address(sock):
+    add = []
+    for line in sock:
+        add.append('address=/%s/#\n' % line)
+    return add
 
-    # 使用 ThreadPoolExecutor 创建线程池，数量为200
-    with ThreadPoolExecutor(max_workers=200) as executor:
-        futures = [executor.submit(process_filter, url) for url in urls]
-        
-        # 显示进度条
-        for future in tqdm(as_completed(futures), total=len(futures), desc="Fetching URLs"):
-            pass  # 等待所有任务完成
-    
-    lines_to_keep = list(set(lines_to_keep))
-
+def process_domains(domain_list):
     valid_domains = []
-    # 使用 ThreadPoolExecutor 再次创建线程池，数量为200
-    with ThreadPoolExecutor(max_workers=200) as executor:
-        futures = [executor.submit(check_dns_resolution, domain) for domain in lines_to_keep]
-        
-        # 显示进度条
-        for future in tqdm(as_completed(futures), total=len(futures), desc="Checking DNS Resolution"):
-            ip = future.result()
-            if ip:
-                valid_domains.append(ip)
+    with tqdm.tqdm(total=len(domain_list), desc='Processing domains') as pbar:
+        with ThreadPoolExecutor(max_workers=99) as executor:
+            future_to_domain = {executor.submit(check_dns_resolution, domain): domain for domain in domain_list}
+            for future in as_completed(future_to_domain):
+                domain = future_to_domain[future]
+                ip = future.result()
+                pbar.update(1)
+                if ip:
+                    valid_domains.append(domain)
+    return valid_domains
 
-    lines_to_keep = filter_domains(valid_domains)
+urls = [
+    'https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt',
+    'https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts',
+    'https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt',
+    'https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt',
+    'https://easylist-downloads.adblockplus.org/easylistchina.txt'
+]
 
-    output_file = '/root/workspace/st/dnsmasq.conf'
-    if lines_to_keep:
+for url in urls:
+    process_filter(url)
+
+lines_to_keep = list(set(lines_to_keep))
+
+output_file = '/root/workspace/st/dnsmasq.conf'
+if lines_to_keep:
+    valid_domains = process_domains(lines_to_keep)
+    lines_to_keep = address(filter_domains(valid_domains))
+
+    if len(lines_to_keep) > 2000:
         with open(output_file, 'w', encoding='utf-8') as f_out:
-            f_out.writelines(line + '\n' for line in lines_to_keep)
+            f_out.writelines(lines_to_keep)
 
-    print(f"Total unique domains written to {output_file}: {len(lines_to_keep)}")
+lines_to_keep = []
